d113499a35fb3ebe7e9617ceb3b7c63340e1fc34
Themi Megas <Tcm4760@gmail.com>
2024-09-21 22:56:29 -0500

    broke up the home script into a common script for across all pages

---
+++ scripts/common.js
@@ -0,0 +1,62 @@
+// crow cursor thing
+const document_wrapper = document.getElementById("document-wrapper");
+document_wrapper.style.cursor = "none";
+const crow_cursor = document.createElement("img");
+crow_cursor.className = "crow-cursor";
+crow_cursor.src = "../images/crow_cursor.svg";
+const crow_cursor_open = document.createElement("img");
+crow_cursor_open.className = "crow-cursor";
+crow_cursor_open.src = "../images/crow_cursor_open.svg";
+document_wrapper.appendChild(crow_cursor);
+document_wrapper.appendChild(crow_cursor_open);
+window.onmousemove = (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+    crow_cursor.style.top = `${y}px`;
+    crow_cursor.style.left = `${x}px`;
+    crow_cursor_open.style.top = `${y}px`;
+    crow_cursor_open.style.left = `${x}px`;
+}
+window.onmouseover = () => {
+    crow_cursor.style.display = "block";
+}
+window.onmouseout = () => {
+    crow_cursor.style.display = "none";
+    crow_cursor_open.style.display = "none";
+}
+const caw_audio = new Audio('../audio/caw1.mp3');
+window.onclick = () => {
+    crow_cursor.style.display = "none";
+    crow_cursor_open.style.display = "block";
+    caw_audio.play();
+    caw_audio.addEventListener("ended", () => {
+        crow_cursor.style.display = "block";
+        crow_cursor_open.style.display = "none";
+    });
+}
+
+//inspect animation
+const inspect_container = document.getElementById("inspect");
+const inspect_images = [];
+document.addEventListener("keydown", async (e) => {
+    if (inspect_images.length === 0) {
+        for (let i = 1; i <= 134; i++) {
+            const img = document.createElement("img");
+            img.src = `../images/inspect/${i}.png`;
+            img.className = "inspect-img";
+            img.decoding = "sync";
+            inspect_images.push(img);
+        }
+    }
+    if (e.key.toLowerCase() === "f") {
+        console.log("clicked button");
+        for (const inspect_image of inspect_images) {
+            inspect_container.appendChild(inspect_image);
+            await sleep((1/15)*1000);
+            inspect_container.removeChild(inspect_image);
+        }
+    }
+});
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
