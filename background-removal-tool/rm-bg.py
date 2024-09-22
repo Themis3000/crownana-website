5d5082e786d99baa1eadf5e07c7d7206f16e2b43
Themi Megas <Tcm4760@gmail.com>
2024-09-21 22:24:59 -0500

    added all sorts of stuff, audio, inspect, etc. inspect broken on first play.

---
+++ background-removal-tool/rm-bg.py
@@ -0,0 +1,27 @@
+import glob
+import cv2
+
+counter = 0
+for path in glob.glob("./imgs/*"):
+    counter += 1
+    src = cv2.imread(path, 1)
+    # Convert image to image gray
+    tmp = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+
+    # Applying thresholding technique
+    _, alpha = cv2.threshold(tmp, 0, 255, cv2.THRESH_BINARY)
+
+    # Using cv2.split() to split channels
+    # of coloured image
+    b, g, r = cv2.split(src)
+
+    # Making list of Red, Green, Blue
+    # Channels and alpha
+    rgba = [b, g, r, alpha]
+
+    # Using cv2.merge() to merge rgba
+    # into a coloured/multi-channeled image
+    dst = cv2.merge(rgba, 4)
+
+    # Writing and saving to a new image
+    cv2.imwrite(f"out/{counter}.png", dst)
--- scripts/home.js
+++ scripts/home.js
@@ -1,3 +1,14 @@
+//welcome audio and lobby
+const background_wrapper = document.getElementById("background_wrapper");
+const welcome = new Audio("../audio/välkommen.m4a");
+const lobby = new Audio('../audio/Charlie\'s Here.wav');
+lobby.loop = true;
+welcome.play();
+welcome.addEventListener("ended", () => {
+   lobby.play();
+   background_wrapper.style.display = "flex";
+});
+
 // banana bread menu thing
 const menu_containers = document.querySelectorAll('.menu-item-container');
 const banana_element_right = document.createElement('img');
@@ -52,4 +63,59 @@ window.onclick = () => {
         crow_cursor.style.display = "block";
         crow_cursor_open.style.display = "none";
     });
+}
+
+
+//hover audios
+const blog_audio = new Audio("../audio/blog.m4a");
+const gallery_audio = new Audio("../audio/gallery.m4a");
+const about_audio = new Audio("../audio/about.m4a");
+const blog_menu = document.getElementById("blog-menu");
+const gallery_menu = document.getElementById("gallery-menu");
+const about_menu = document.getElementById("about-menu");
+blog_menu.addEventListener("mouseenter", () => {
+   blog_audio.play();
+});
+blog_menu.addEventListener("mouseleave", () => {
+    blog_audio.pause();
+    blog_audio.fastSeek(0);
+});
+gallery_menu.addEventListener("mouseenter", () => {
+   gallery_audio.play();
+});
+gallery_menu.addEventListener("mouseleave", () => {
+    gallery_audio.pause();
+    gallery_audio.fastSeek(0);
+});
+about_menu.addEventListener("mouseenter", () => {
+   about_audio.play();
+});
+about_menu.addEventListener("mouseleave", () => {
+    about_audio.pause();
+    about_audio.fastSeek(0);
+});
+
+//inspect animation
+const inspect_container = document.getElementById("inspect");
+const inspect_images = [];
+for (let i = 1; i <= 134; i++) {
+    const img = document.createElement("img");
+    img.src = `../images/inspect/${i}.png`;
+    img.className = "inspect-img";
+    inspect_images.push(img);
+}
+document.addEventListener("keydown", async (e) => {
+    console.log("click");
+    if (e.key === "f") {
+        console.log("clicked button");
+        for (const inspect_image of inspect_images) {
+            inspect_container.appendChild(inspect_image);
+            await sleep((1/15)*1000);
+            inspect_container.removeChild(inspect_image);
+        }
+    }
+});
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
