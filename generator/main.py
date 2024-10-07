129dde2e36764a4c9ce2aba5469c8ff20eb49eb9
Themi Megas <Tcm4760@gmail.com>
2024-10-07 00:30:29 -0500

    added gallery categories

--- generator/main.py
+++ generator/main.py
@@ -5,13 +5,19 @@ env = Environment(
     autoescape=select_autoescape()
 )
 
+
 # Generate gallery documents
 template = env.get_template("gallery.jinja2")
 gallery_config = yaml.safe_load(open("../gallery_config.yml"))
 all_entries = []
+
 for category_name in gallery_config:
     category = gallery_config[category_name]
     all_entries.extend(category)
-template_content = template.render(img_entries=all_entries, selected="all")
-with open("../public/gallery/home.html", "w") as f:
-    f.write(template_content)
+    category_content = template.render(img_entries=category, selected=category_name)
+    with open(f"../public/gallery/{category_name}.html", "w") as f:
+        f.write(category_content)
+
+all_content = template.render(img_entries=all_entries, selected="all")
+with open("../public/gallery/index.html", "w") as f:
+    f.write(all_content)
