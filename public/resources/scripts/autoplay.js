afad9924ae0922358ae8be7562d5ef618f5d15a3
Themi Megas <Tcm4760@gmail.com>
2025-05-11 20:33:50 -0500

    Used clipping instead for star animation, works properly one way.

--- public/resources/scripts/autoplay.js
+++ public/resources/scripts/autoplay.js
@@ -1,3 +1,13 @@
-const autoplayEvent = new Event("autoplay-allowed");
+// const autoplayEvent = new Event("autoplay-allowed");
+// const star = document.getElementById("autoplay-img");
+//
+// star.addEventListener("animationend", () => {
+//     console.log("animation end");
+//     // setTimeout(() => {
+//     //     console.log("inverting animation");
+//     //     star.style.transform = "translate(-50%, -50%) scale(5)";
+//     //     star.style.animation = "grow 1s ease-in both reverse";
+//     // }, 4000);
+// });
 
 // document.body.dispatchEvent(autoplayEvent);
