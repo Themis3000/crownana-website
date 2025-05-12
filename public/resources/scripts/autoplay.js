90816b840f2239a6ad7cf23117884e612dcd503d
Themi Megas <Tcm4760@gmail.com>
2025-05-12 01:08:07 -0500

    added content to the popup window

--- public/resources/scripts/autoplay.js
+++ public/resources/scripts/autoplay.js
@@ -1,13 +1,8 @@
-// const autoplayEvent = new Event("autoplay-allowed");
-// const star = document.getElementById("autoplay-img");
-//
-// star.addEventListener("animationend", () => {
-//     console.log("animation end");
-//     // setTimeout(() => {
-//     //     console.log("inverting animation");
-//     //     star.style.transform = "translate(-50%, -50%) scale(5)";
-//     //     star.style.animation = "grow 1s ease-in both reverse";
-//     // }, 4000);
-// });
+const autoplayEvent = new Event("autoplay-allowed");
+const star = document.getElementById("star");
+
+star.addEventListener("animationend", () => {
+    console.log("animation end");
+});
 
 // document.body.dispatchEvent(autoplayEvent);
