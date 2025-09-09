d453b2908b7463b894f72586de456f738d7e50c1
Themi Megas <Tcm4760@gmail.com>
2025-09-09 18:50:53 -0500

    Started dealing with storing sizing rules.

--- generator/css/css_sizing_parser.py
+++ generator/css/css_sizing_parser.py
@@ -1,6 +1,7 @@
 import dataclasses
 import tinycss2
-from typing import Dict
+from tinycss2.ast import Node
+from typing import Dict, List, Self
 
 
 @dataclasses.dataclass
@@ -11,6 +12,21 @@ class SizingRule:
     def __post_init__(self):
         if self.widthPx is None and self.heightPx is None:
             raise Exception("Initiated SizingRule with empty height and width.")
+
+
+@dataclasses.dataclass
+class NamedSizingRule:
+    name: str
+    sizing_rule: SizingRule | None
+    parent_rules: Dict[str, Self]
+
+    def check_for_parent(self, parent_name: str) -> Self | None:
+        return self.parent_rules.get(parent_name, None)
+
+    def store_child(self, parent: Self):
+        if parent.name in self.parent_rules:
+            raise Exception("Duplicate rules detected... Uhhh whatdoido?")
+        self.parent_rules[parent.name] = parent
 
 
 class CSSSizingParser:
@@ -39,6 +55,7 @@ class CSSSizingParser:
 
             if width or height:
                 sizing_rule = SizingRule(widthPx=width, heightPx=height)
+                self._store_sizing_by_prelude(sizing_rule=sizing_rule, prelude=rule.prelude)
                 print(sizing_rule)
 
     @staticmethod
@@ -51,3 +68,49 @@ class CSSSizingParser:
         if token.lower_unit != "px":
             return
         return token.int_value
+
+    def _store_sizing_by_prelude(self, sizing_rule: SizingRule, prelude: List[Node]):
+        ###
+        # The following need to be implemented
+        # .thing .other-thing
+        # selects all .other-thing in thing
+        # .thing img
+        # selects all img in .thing
+        # .thing
+        # #thing
+        ####
+
+        # Describes the inheritance of the name to be written out.
+        # For example, prelude ".writing-style img" would turn into [".writing-style", img]
+        name_inheritance_out = []
+
+        prelude_iter = iter(prelude)
+        for token in prelude_iter:
+            # Skip all white spaces. No need to deal with them currently
+            if isinstance(token, tinycss2.tokenizer.WhitespaceToken):
+                continue
+
+            # Is a comma token
+            if isinstance(token, tinycss2.tokenizer.LiteralToken) and token.value == ",":
+                self._store_sizing_by_prelude(sizing_rule=sizing_rule, prelude=list(prelude_iter))
+                # Break largely for emphasis to the programmer that prelude_iter is now exhausted and no more
+                # iterations will occur.
+                break
+
+            # Cannot support an inheritance depth of more than 1 at this time.
+            # It's unnecessary for the current scope to deal with.
+            # Intentionally placed after the comma token check and whitespace check.
+            if len(name_inheritance_out) == 2:
+                break
+
+            # Is a class definition
+            if isinstance(token, tinycss2.tokenizer.LiteralToken) and token.value == ".":
+                class_name_token = next(prelude_iter)
+                if not isinstance(class_name_token, tinycss2.tokenizer.IdentToken):
+                    raise Exception("Parsing error! Expected class name ident.")
+                print(class_name_token)
+                continue
+
+            # Is an id
+            if isinstance(token, tinycss2.tokenizer.HashToken):
+                print(token)
